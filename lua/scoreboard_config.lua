90e6797bf58b4e2be96fedb1378f476033fe2fd4
HarryCallahan308 <jringel2@gmail.com>
2024-07-21 12:59:18 -0400

    Update scoreboard_config.lua

--- lua/scoreboard_config.lua
+++ lua/scoreboard_config.lua
@@ -6,15 +6,15 @@ camelCaseScoreboardConfig.Discord = 'https://discord.gg/GJ5uzNSM'
 camelCaseScoreboardConfig.Steam = 'https://steamcommunity.com/id/camelCase807/'
 
 camelCaseScoreboardConfig.Ranks = {
-    ['superadmin'] = {name='Super Administrator', color = Color(0,0,255)},
-    ['developer'] = {name='Developer', color = Color(0,0,255)},
-    ['moderator'] = {name='Moderator', color = Color(255,0,0)},
-    ['vip'] = {name='VIP', color = Color(0,255,0)},
-    ['user'] = {name='User', color = Color(0,255,0)}
+    //['superadmin'] = {name='Super Administrator', color = Color(0,0,255)},
+    //['developer'] = {name='Developer', color = Color(0,0,255)},
+    //['moderator'] = {name='Moderator', color = Color(255,0,0)},
+    //['vip'] = {name='VIP', color = Color(0,255,0)},
+    //['user'] = {name='User', color = Color(0,255,0)}
 } 
 camelCaseScoreboardConfig.Staff = {
-    ['superadmin'] = true,
-    ['developer'] = true,
-    ['moderator'] = true
+    //['superadmin'] = true,
+    //['developer'] = true,
+    //['moderator'] = true
 } 
  
